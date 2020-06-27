1d7505934bd81dcc73b3a1c3c7aed04674049de2
Kai_Xuan233 <likaiyue0989@foxmail.com>
2020-06-27 20:13:31 +0800

    fixed filename bug

--- backup.sh
+++ backup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # source: https://github.com/Lilium-Studio/minecraft-server-backup-script
 
-# 存档路径
-save=./world/
-
 date=$(date --iso-8601=minutes)
 day=${date:0:10}
 hour=${date:11:2}
 minute=${date:14:2}
 echo 开始备份...
-zip -r world_$day-$hour-$minute.zip $save
-# mv world_$day-$hour-$minute.zip ./backups/
-echo 备份完成，已保存至world_$day-$hour-$minute.zip
+
+# 存档路径
+save=./world/
+# 备份文件名称
+file=world_$day-$hour-$minute
+
+zip -r $file.zip $save
+# mv $file.zip ./backups/
+echo 备份完成，已保存至$file.zip
